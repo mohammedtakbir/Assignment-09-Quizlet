46f4842d116c7b6fa6c15e0b8e615adf8c96adde
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-11 10:19:32 +0600

    quiz route added

--- src/components/QuizTopics/QuizTopics.js
+++ src/components/QuizTopics/QuizTopics.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { ArrowRightIcon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 const quizTopics = ({ quizTopic }) => {
-    const { logo, name, total } = quizTopic;
-    console.log(quizTopic)
+    const { logo, name, total, id } = quizTopic;
     return (
         <div className='p-5 shadow-md rounded-md'>
             <img className='bg-[#ddd] rounded-md' src={logo} alt="" />
             <p className='text-2xl font-medium mt-4 mb-3'>{name}</p>
             <p className='font-medium'>Total Quiz: {total}</p>
-            <button className='bg-teal-400 py-2 px-5 text-white font-medium rounded-lg text-lg mt-4'>
-                <span>Start Practice</span>
-                <ArrowRightIcon className="h-5 w-5 text-white inline ml-1"/>
-            </button>
+            <Link to={`/quiz/${id}`}>
+                <button className='bg-teal-400 py-2 px-5 text-white font-medium rounded-lg text-lg mt-4'>
+                    <span>Start Practice</span>
+                    <ArrowRightIcon className="h-5 w-5 text-white inline ml-1" />
+                </button>
+            </Link>
         </div>
     );
 };
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog/Blog";
 import Home from "../components/Home/Home";
+import Quiz from "../components/Quiz/Quiz";
 import Statistics from "../components/Statistics/Statistics";
 import Main from "../layout/Main";
 
@@ -26,6 +27,11 @@ export const router = createBrowserRouter([
             {
                 path: '/blog',
                 element: <Blog />
+            },
+            {
+                path: '/quiz/:id',
+                loader: ({params}) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`),
+                element: <Quiz />
             }
         ]
     }
