862c33a15661b10649fed44db8a3ccac28a4ef57
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-11 11:36:58 +0600

    show correct answer when press the eye button

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,25 +2,33 @@ import React from 'react';
 import Option from '../Option/Option';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const Question = ({q}) => {
-    const {options, question, correctAnswer } = q;
+import { EyeIcon } from '@heroicons/react/24/solid';
+const Question = ({ q }) => {
+    const { options, question, correctAnswer } = q;
+
     const handleTrueFalse = (selectedOption) => {
-        if(selectedOption === correctAnswer){
-            toast.success('true', {autoClose: 500})
-        }else{
-            toast.error('false', {autoClose: 500})
+        if (selectedOption === correctAnswer) {
+            toast.success('true', { autoClose: 500 })
+        } else {
+            toast.error('false', { autoClose: 500 })
         }
-    }
+    };
+    const handleCorrectAns = (correctAns) => {
+        toast.info((correctAns), {autoClose: 2000});
+    };
     return (
-        <div className='max-w-[800px] mx-auto shadow-lg px-7 py-5 my-10 rounded-lg'>
-            <p className='text-2xl font-medium text-center pb-5 pt-10 text-teal-500'>{question}</p>
+        <div className='max-w-[800px] mx-auto shadow-lg px-7 py-5 my-10 rounded-lg relative'>
+                <p className='text-2xl font-medium text-center pb-5 pt-10 text-teal-500'>{question}</p>
+                <button onClick={() => handleCorrectAns(correctAnswer)} className='absolute top-[30px] right-[20px]'>
+                    <EyeIcon className="h-5 w-5 text-black inline ml-1" />
+                </button>
             <div>
                 {
                     options.map((option, index) => <Option
                         option={option}
                         key={index}
                         handleTrueFalse={handleTrueFalse}
-                        />)
+                    />)
                 }
             </div>
         </div>
