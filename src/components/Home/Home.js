1064cdbbb41fe94fa7cb91388d7919b4a17b06f1
mohammedtakbir <takbirhossain678@gmail.com>
2022-10-11 08:21:23 +0600

    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import headerImg from '../../images/header--img.jpg'
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='container mx-auto'> 
+            <div className='mt-10 flex items-center gap-10 p-10'>
+                <img className='w-full' src={headerImg} alt="" />
+                <div>
+                    <h2 className='font-bold text-4xl mb-10'>Organize Live Multiplayer Play Buzz Quiz for Your Event!</h2>
+                    <p className='font-medium text-xl mb-7'>
+                        myQuiz is a web-based Audience Engagement Cloud Platform for hosting interactive trivia quizzes at in-person, virtual, and hybrid events. No app install is required
+                    </p>
+                    <div className='flex'>
+                        <button className='bg-teal-400 py-3 px-7 mr-8 text-white font-medium rounded-lg inline-block'>Pick a plan</button>
+                        <button className='bg-teal-400 py-3 px-7 text-white font-medium rounded-lg inline-block'>Demo Quiz</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
